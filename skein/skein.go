c1e1d488ea96c8c92b2d5d64c99ef875606de3bc
Mark Johnson <amckhome@tpg.com.au>
2012-04-10 23:42:17 +1000

    fix skein c reset

--- skein/skein.go
+++ skein/skein.go
@@ -25,8 +25,7 @@ func NewSkein512() *Skein512 {
 var d []byte = []byte("ND3EAJ.;1QDGLXV)G>B8-1*R9=GK(6XC")
 
 func (t *Skein512) Hash(data []byte) []int8 {
-	//t.c = make([]int64, 9) //reset c (final blocked vals)
-	t.c = t.c[:0]
+	t.c = make([]int64, 9) //reset c (final blocked vals)
 	fmt.Println(t.c)
 	co1 := uint(56)
 
