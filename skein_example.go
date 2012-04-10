9d6eed39cb375e08484b2280bfa9631d9b4a7c3d
Mark Johnson <amckhome@tpg.com.au>
2012-04-10 20:21:42 +1000

    go fmt

--- skein_example.go
+++ skein_example.go
@@ -1,18 +1,19 @@
 package main
+
 import (
-    "fmt"
-    "github.com/farces/skein512/skein"
-    "encoding/hex"
+	"encoding/hex"
+	"fmt"
+	"github.com/farces/skein512/skein"
 )
 
 func main() {
-    x := skein.NewSkein512()
-    r := x.Hash([]byte{0xFF})
-    out := make([]byte,64)
+	x := skein.NewSkein512()
+	r := x.Hash([]byte{0xFF})
+	out := make([]byte, 64)
 
-    for i := 0; i<64; i++ {
-        out[i] = byte(r[i])
-    }
+	for i := 0; i < 64; i++ {
+		out[i] = byte(r[i])
+	}
 
-    fmt.Println(hex.EncodeToString(out))
+	fmt.Println(hex.EncodeToString(out))
 }
